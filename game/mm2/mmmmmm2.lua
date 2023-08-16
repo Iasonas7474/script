39e328e9f9d2a5c96a999e6c0d667f7195818f96
Iasonas7474 <117999180+Iasonas7474@users.noreply.github.com>
2023-08-16 17:17:11 +0200

    Update mmmmmm2.lua

--- game/mm2/mmmmmm2.lua
+++ game/mm2/mmmmmm2.lua
@@ -5,10 +5,10 @@ local Window = Library.CreateLib("mm2 game", "Ocean")
 local game = Window:NewTab("mm2 game")
 local gameSection = game:NewSection("mm2 game")
 
-gameSection:NewButton("ButtonText", "ButtonInfo", function()
+gameSection:NewButton("mm2 admin", "kein", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Iasonas7474/script/main/mmm2.lua"))()
 end)
 
-gameSection:NewButton("ButtonText", "ButtonInfo", function()
+gameSection:NewButton("mm2 game", "mm2 script", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Iasonas7474/script/main/mm2%20script.lua"))()
 end)
